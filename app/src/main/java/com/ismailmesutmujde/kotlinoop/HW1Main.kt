08ba087e7e1557ffc3071d1d31acacdda5834c76
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-16 17:35:25 +0300

    updated: homework 1 - temperature conversion

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW1Main.kt
@@ -0,0 +1,7 @@
+package com.ismailmesutmujde.kotlinoop
+
+fun main() {
+    val h1 = HW1TemperatureConversion()
+    val result = h1.convert(30.0)
+    println("Fahrenheit : $result")
+}
---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW1TemperatureConversion.kt
@@ -0,0 +1,10 @@
+package com.ismailmesutmujde.kotlinoop
+
+
+class HW1TemperatureConversion {
+
+    fun convert(degree:Double) : Double {
+        val fahrenheit = degree * 1.8 + 32
+        return fahrenheit
+    }
+}
