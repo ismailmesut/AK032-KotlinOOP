3b5448033c82630ef573a242a38c35d7f69080cd
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-16 18:20:19 +0300

    updated: homework 5 - interior angle sum

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW5InteriorAngleSum.kt
@@ -0,0 +1,9 @@
+package com.ismailmesutmujde.kotlinoop
+
+class HW5InteriorAngleSum {
+
+    fun interiorAngleSum(numberOfSides : Int) : Int {
+        val sum = (numberOfSides-2)*180
+        return sum
+    }
+}
---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW5Main.kt
@@ -0,0 +1,7 @@
+package com.ismailmesutmujde.kotlinoop
+
+fun main() {
+    val h5 = HW5InteriorAngleSum()
+    val result = h5.interiorAngleSum(3)
+    println("Interior Angle Sum : $result")
+}
