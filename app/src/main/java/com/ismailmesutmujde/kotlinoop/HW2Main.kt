f271952f0e6e41ebb906688c978aa1ac9912ee61
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-16 17:41:23 +0300

    updated: homework 2 - perimeter calculation

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW2Main.kt
@@ -0,0 +1,6 @@
+package com.ismailmesutmujde.kotlinoop
+
+fun main() {
+    val h2 = HW2PerimeterCalculation()
+    h2.perimeter(30,40)
+}
---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop/HW2PerimeterCalculation.kt
@@ -0,0 +1,9 @@
+package com.ismailmesutmujde.kotlinoop
+
+class HW2PerimeterCalculation {
+
+    fun perimeter(shorSide:Int, longSide:Int) {
+        val perimeterCalculation = 2 * shorSide + 2 * longSide
+        println("Rectangle Perimeter : $perimeterCalculation")
+    }
+}
